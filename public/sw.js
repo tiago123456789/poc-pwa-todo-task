72ae620a9908025311a062cf77c9a6bd9617a9a2
Tiago rosa da costa <tiagorosadacost@gmail.com>
2021-09-12 19:58:44 -0300

    Changes

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,30 @@
-var CACHE_NAME = 'otimigas-app';
+importScripts('https://unpkg.com/workbox-sw@0.0.2/build/importScripts/workbox-sw.dev.v0.0.2.js');
+importScripts('https://unpkg.com/workbox-runtime-caching@1.3.0/build/importScripts/workbox-runtime-caching.prod.v1.3.0.js');
+importScripts('https://unpkg.com/workbox-routing@1.3.0/build/importScripts/workbox-routing.prod.v1.3.0.js');
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(self.skipWaiting());
+const assetRoute = new workbox.routing.RegExpRoute({
+    regExp: new RegExp('/static/**/*.(html|js|css|png|jpg)'),
+    handler: new workbox.runtimeCaching.CacheFirst()
+});
+
+const router = new workbox.routing.Router();
+router.registerRoutes({routes: [assetRoute]});
+router.setDefaultHandler({
+    handler: new workbox.runtimeCaching.CacheFirst()
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+
+        caches.match(event.request)
+            .then(function (response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+            )
+    );
 });
 
 self.addEventListener('message', (event) => {
