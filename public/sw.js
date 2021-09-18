506ac1136dc93f8de6c04eb28c0e8eb0e7e53fb5
Tiago rosa da costa <tiagorosadacost@gmail.com>
2021-09-18 17:34:32 -0300

    Changes file service work to resolve problem cache files in production

--- public/sw.js
+++ public/sw.js
@@ -1,56 +1,39 @@
-importScripts('https://unpkg.com/workbox-sw@0.0.2/build/importScripts/workbox-sw.dev.v0.0.2.js');
-importScripts('https://unpkg.com/workbox-runtime-caching@1.3.0/build/importScripts/workbox-runtime-caching.prod.v1.3.0.js');
-importScripts('https://unpkg.com/workbox-routing@1.3.0/build/importScripts/workbox-routing.prod.v1.3.0.js');
+var CACHE_NAME = 'todo-app';
 
-const assetRoute = new workbox.routing.RegExpRoute({
-    regExp: new RegExp('/static/**/*.(html|js|css|png|jpg)'),
-    handler: new workbox.runtimeCaching.CacheFirst()
-});
-
-const router = new workbox.routing.Router();
-router.registerRoutes({routes: [assetRoute]});
-router.setDefaultHandler({
-    handler: new workbox.runtimeCaching.CacheFirst()
-});
-
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-
-        caches.match(event.request)
-            .then(function (response) {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            }
-            )
-    );
-});
-
-self.addEventListener('message', (event) => {
-    if (event.data.type === 'CACHE_URLS') {
-        event.waitUntil(
-            caches.open(KEY)
-                .then( (cache) => {
-                    return cache.addAll(event.data.payload);
-                })
-        );
-    }
-});
-
-self.addEventListener('fetch', function (event) {
+self.addEventListener('install', function (event) {
     
-    event.respondWith(
+});
 
-        caches.match(event.request)
-            .then(function (response) {
-                // Cache hit - return response
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
+// Cache files
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            if (response) {
+                return response;
+            } else {
+                return fetch(event.request)
+                    .then(function (res) {
+                        return caches.open(CACHE_NAME).then(function (cache) {
+                            cache.put(event.request.url, res.clone());
+                            return res;
+                        });
+                    })
+
             }
-            )
+        })
     );
 });
 
+// Delete files cached when new service worker is installed
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys()
+            .then(function (keyList) {
+                return Promise.all(keyList.map(function (key) {
+                    return caches.delete(key);
+
+                }));
+            })
+    );
+    return self.clients.claim();
+});
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { registerServiceWorker } from "./ serviceWorker"
+import { registerServiceWorker } from "./serviceWorker"
 
 ReactDOM.render(
   <React.StrictMode>
